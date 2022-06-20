35fc3a5e9caa6692c47cd0d700f9d7ff052c046c
Vitali Skarbinski <sfc-ruper@mail.ru>
2022-06-20 20:26:58 +0300

    feat: add media queries for tablet and burger menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,23 @@
+let burgIcon = document.querySelector('.burger-menu-icon');
+let burgMenu = document.querySelector('.header__nav');
+if (burgIcon) {
+
+  burgIcon.addEventListener("click", function (e) {
+    burgMenu.classList.toggle('active');
+    burgIcon.classList.toggle('active');
+
+  })
+}
+burgMenu.addEventListener('click', e => {
+  if (e.target) {
+    burgMenu.classList.toggle('active');
+    burgIcon.classList.toggle('active');
+  }
+});
+
+
+
+
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   spaceBetween: 5,
